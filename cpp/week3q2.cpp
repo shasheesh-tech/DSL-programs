3ec8f6e57c116de108a043f3924872b43e5306a8
kamanaboina shasheesh <shasheesh20@gmail.com>
2026-02-27 11:19:07 +0530

    Update week3q2.cpp

--- cpp/week3q2.cpp
+++ cpp/week3q2.cpp
@@ -2,24 +2,18 @@
 #include <iostream>
 #include <list>
 using namespace std;
-
 int main() {
     list<string> l;
-
     l.push_back("C++");
     l.push_back("Java");
     l.push_back("Python");
-
-    cout << "Initial List:\n";
+    cout<<"Initial List:\n";
     for(list<string>::iterator it = l.begin(); it != l.end(); it++)
-        cout << *it << " ";
-
+        cout<<*it<<" ";
     l.push_front("DS");
     l.pop_back();
-
-    cout << "\n\nAfter Insertion and Deletion:\n";
+    cout <<"\n\nAfter Insertion and Deletion:\n";
     for(list<string>::iterator it = l.begin(); it != l.end(); it++)
-        cout << *it << " ";
-
+        cout<<*it<< " ";
     return 0;
 }
