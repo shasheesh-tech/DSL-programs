4a60fa44098f858b1ebae1a58ef1c7eebb553714
kamanaboina shasheesh <shasheesh20@gmail.com>
2026-02-27 11:16:34 +0530

    Update week3q5.cpp

--- cpp/week3q5.cpp
+++ cpp/week3q5.cpp
@@ -1,39 +1,28 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>  // For sort, find, count
+#include <algorithm>  
 using namespace std;
-
 int main() {
     vector<int> v;
-    int n, element, searchValue, countValue;
-
-    // Input number of elements
-    cout << "Enter number of elements: ";
-    cin >> n;
-
-    // Insert elements into vector
+    int n,element,searchValue,countValue;
+    cout<< "Enter number of elements: ";
+    cin>>n;
     cout << "Enter " << n << " elements: ";
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         cin >> element;
         v.push_back(element);
     }
-
-    // Display original vector
-    cout << "\nOriginal Vector: ";
-    for (int x : v) {
+    cout<< "\nOriginal Vector: ";
+    for(int x : v) {
         cout << x << " ";
     }
-
-    // 1. SORT algorithm
     sort(v.begin(), v.end());
     cout << "\nSorted Vector: ";
-    for (int x : v) {
+    for(int x : v) {
         cout << x << " ";
     }
-
-    // 2. FIND algorithm
-    cout << "\n\nEnter element to find: ";
-    cin >> searchValue;
+    cout<< "\n\nEnter element to find: ";
+    cin>> searchValue;
 
     auto it = find(v.begin(), v.end(), searchValue);
     if (it != v.end()) {
@@ -42,8 +31,6 @@ int main() {
     } else {
         cout << "Element not found in vector." << endl;
     }
-
-    // 3. COUNT algorithm
     cout << "Enter element to count occurrences: ";
     cin >> countValue;
 
@@ -51,4 +38,5 @@ int main() {
     cout << "Element " << countValue << " occurs " << c << " times in vector." << endl;
 
     return 0;
+
 }
